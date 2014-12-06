7c19738b9902ea040d659c22638ef55910e69c2a
Tejas <tejaskale27@gmail.com>
2014-12-07 01:59:44 +0530

    test

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\workarea\AndroidDevelopment\hello\platforms\android\ant-build\AndroidManifest.xml : \
-D:\workarea\AndroidDevelopment\hello\platforms\android\AndroidManifest.xml \
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-D:\workarea\AndroidDevelopment\hello\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
-D:\workarea\AndroidDevelopment\hello\platforms\android\ant-build\CordovaApp.ap_ \
-D:\workarea\AndroidDevelopment\hello\platforms\android\ant-build\classes.dex \
-D:\workarea\AndroidDevelopment\hello\platforms\android\CordovaLib\ant-build\classes.jar \
